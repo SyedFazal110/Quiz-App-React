b75e0359e4ea63045be6b0d3da0eca3d6aa35d62
SyedFazal110 <syedfazal808@gmail.com>
2024-09-08 22:58:09 +0500

    pending

--- Quiz App/src/App.jsx
+++ Quiz App/src/App.jsx
@@ -1,12 +1,37 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ question , setQuestion] = useState([]);
+  const [ questionState , setQuestionState ] = useState(0)
+
+  useEffect(() => {
+    axios('https://the-trivia-api.com/v2/questions')
+    .then((res) => {
+      console.log(res.data)
+      setQuestion(res.data)
+    }).catch((error) => {
+      console.log(error);
+    })
+  } , [])
+
+  function next(index){
+
+  }
+
 
   return (
     <>
-      
+      <h1>Quiz App</h1>
+      { question.length > 0 ? <div>
+      <h1>Q{questionState + 1} : {question[questionState].question.text}</h1>
+      <ol>
+        <li>
+          <input type="radio" name="choice" id="" />
+        </li>
+      </ol>
+      <button onClick={() =>{next}}>Next</button>
+      </div> : <h1>Loading....</h1>}
     </>
   )
 }
